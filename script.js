ca45359705bd3c5db200e220fd02c3fbb09d6c3a
Rose Peruski <rose.peruski@detroitlabs.com>
2017-05-09 10:41:40 -0400

    adding js file and json database

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('#more').click(function() {
+		//move to next kitty
+		alert('this will move eventually');
+	});
+
+	$('#add').click(function() {
+		//open up add functions
+		alert('Coming soon!');
+	})
+});
