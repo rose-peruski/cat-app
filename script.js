24c3fca81b8457cc2503258af9198484f9fb46cd
Rose Peruski <rose.peruski@detroitlabs.com>
2017-05-10 14:13:22 -0400

    able to write to database

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var mykey=config.MY_KEY;
 var myDatabase = config.databaseURL;
 var myStorage = config.storageBucket;
-
+var kittyWord;
+var kittyTranslation;
+var count = 10;
 
 
 $(document).ready(function(){
@@ -16,21 +18,39 @@ $(document).ready(function(){
 	$('#finish-add').hide();
 	$('#add').click(function() {
 		//open up add functions
+		count++;
 		$('#userCat').show().keyup(function() {
-
-                // env=$('input[name=otherAdd]').val();
-                // console.log(env);
+			kittyWord=$('#userCat').val();
+			console.log(kittyWord);
             });
 		$('#english-trans').show().keyup(function() {
-                // env=$('input[name=otherAdd]').val();
-                // console.log(env);
+               kittyTranslation=$('#english-trans').val();
+			console.log(kittyTranslation);
             });
-		$('#finish-add').show()
+		$('#finish-add').show().click(function() {
+
+			catCreate = new Cat(kittyWord, kittyTranslation, count);
+			console.log(catCreate);
+			count++
+
+				ref.push({
+		      cat: kittyWord,
+		      english: kittyTranslation,
+		      num: count
+		    })
+		});
 	})
 
 	// Get a reference to the database service
 
 	
 });
+
+function Cat(word, engTrans, num) {
+	this.word=word;
+	this.engTrans=engTrans;
+	this.num=num;
+
+}
 
 
