0f68315a01672b163d63c451b2f3a1eff585ece5
Rose Peruski <rose.peruski@detroitlabs.com>
2017-05-10 13:12:32 -0400

    adding anonymous login

--- script.js
+++ script.js
@@ -10,10 +10,22 @@ $(document).ready(function(){
 		window.location='index.html';
 		
 	});
-
+	
+	$('#userCat').hide()
+	$('#english-trans').hide();
+	$('#finish-add').hide();
 	$('#add').click(function() {
 		//open up add functions
-		alert('Coming soon!');
+		$('#userCat').show().keyup(function() {
+
+                // env=$('input[name=otherAdd]').val();
+                // console.log(env);
+            });
+		$('#english-trans').show().keyup(function() {
+                // env=$('input[name=otherAdd]').val();
+                // console.log(env);
+            });
+		$('#finish-add').show()
 	})
 
 	// Get a reference to the database service
